73ee348a027d477ec00ac3cf40094bfdf4a24f97
Ram <rajeshchaganti89@gmail.com>
2021-04-28 13:22:46 +0530

    ems

---
+++ EMS/src/com/mph/Exception/EmployeeNotFoundException.java
@@ -0,0 +1,15 @@
+package com.mph.Exception;
+
+public class EmployeeNotFoundException {
+
+	public EmployeeNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public String toStringg() {
+		return "EmployeeNotFoundException []";
+	}
+	
+}
